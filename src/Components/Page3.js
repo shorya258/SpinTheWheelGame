33909d4f11759e0ddca3ab5b2398edfddbde61cd
Shorya Hayaran <Shoryahayaran@gmail.com>
2023-03-23 02:33:38 +0530

    added media queries for third page

--- src/Components/Page3.js
+++ src/Components/Page3.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useMediaQuery } from "react-responsive";
 import wheel from "../images/well 2.png";
+import tabletWheel from "../images/medWheel.png";
 import mobilewheel from "../images/mobilewell.png";
 import { useLocation } from "react-router-dom";
 import { Button } from "@mui/material";
@@ -8,28 +9,41 @@ import "../page3.css";
 const Page3 = (props) => {
   const location = useLocation();
   const myCode = location.state.code;
-  // let { result } = props;
-  // (props.location && props.location.state) || {};
   console.log("props pg3", location.state.code, location.state.result);
 
   const handleCopyText = () => {
+    // wcopy discount code to the clipboard
     navigator.clipboard.writeText(myCode);
-    console.log(myCode);
     window.alert("coupon copied!");
   };
+  // assigning different screen widths to different variables to display respective image for respective device later
   const isDesktopOrLaptop = useMediaQuery({
-    query: "(min-width: 1224px)",
+    query: "(max-width: 5524px)",
   });
-  const isTabletOrMobile = useMediaQuery({ query: "(max-width: 800px)" });
+
+  const isTablet = useMediaQuery({
+    query: "(max-width: 1180px)",
+  });
+  const isMobile = useMediaQuery({ query: "(max-width: 800px)" });
   return (
     <div className="container box">
       <div className="wheel">
+        {/* checks if device is laptop and if yes then show image for laptop */}
         {isDesktopOrLaptop && (
           <img src={wheel} className="bigWheel" alt="spin-wheel"></img>
         )}
       </div>
+
+      {/* checks if device is tablet and if yes then show image for tablet */}
+      <div className="tabletWheel">
+        {isTablet && (
+          <img src={tabletWheel} className="medWheel" alt="spin-wheel"></img>
+        )}
+      </div>
+
+      {/* checks if device is mobile and if yes then show image for mobile */}
       <div className="mobilewheel">
-        {isTabletOrMobile && (
+        {isMobile && (
           <img src={mobilewheel} className="smallWheel" alt="spin-wheel"></img>
         )}
       </div>
@@ -37,9 +51,11 @@ const Page3 = (props) => {
         <div className="congoMsg">
           <p>Congrats you have won:</p>
         </div>
+        {/* result of the spin wheel sent as a prop from second page */}
         <h2> {location.state.result}</h2>
         <div className="couponCode">
           <div className="myCode">
+            {/* discount code from the result of the spin wheel game */}
             <p>{myCode}</p>
           </div>
           <div className="myCopyBtn">
@@ -59,7 +75,7 @@ const Page3 = (props) => {
         >
           Close panel and copy
         </Button>
-        <div>
+        <div className="claimDiscount">
           <p>
             <i>* you can claim discount only for 10 minutes</i>
           </p>
